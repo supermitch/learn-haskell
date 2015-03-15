5c955ecbf39002c57b3d87472ee5df6f759d60df
Mitch LeBlanc <supermitch@gmail.com>
2015-03-14 21:33:59 -0700

    Complete patterns section in Chapter 4

--- learn-you-a-haskell/chap-4.hs
+++ learn-you-a-haskell/chap-4.hs
@@ -18,11 +18,44 @@ charName 'a' = "Albert"
 charName 'm' = "Mitch"
 
 -- Non pattern matched method
-addVectors :: (Num a) => (, a) -> (a, a) -> (a, a)
-addVectors a b = (fst a + fst b, snd a + snd b)
+--addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+--addVectors a b = (fst a + fst b, snd a + snd b)
 
 -- Pattern matching way
-addVectors :: (Num a) => (, a) -> (a, a) -> (a, a)
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+-- Custom 'head' implementation
+head' :: [a] -> a
+head' [] = "You can't head an empty list"
+head' (x:xs) = x
+
+-- Custom recursive 'length' implementation
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+-- Custom recursive 'sum' implementation
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+-- Variable 'patterns'
+firstLetter :: String -> String
+firstLetter "" = "Empty string input is no good"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+-- all@ saves us having to retype (x:xs), we've named that pattern "all"
+
+-- Guards:
 
 
