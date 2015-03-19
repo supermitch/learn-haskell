928bef7a619abae237c5fb9c67e5e11ef52c338b
Mitch LeBlanc <supermitch@gmail.com>
2015-03-18 21:09:41 -0700

    Finish chap-4 guards

--- learn-you-a-haskell/chap-4.hs
+++ learn-you-a-haskell/chap-4.hs
@@ -56,6 +56,72 @@ firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
 -- all@ saves us having to retype (x:xs), we've named that pattern "all"
 
--- Guards:
+--  Guards:
+-- =========
 
+bmiTell :: (RealFloat a) => a -> String
+bmitell bmi
+    | bmi <= 18.5 = "You're underweight."
+    | bmi <= 25.0 = "You're normal."
+    | bmi <= 30.0 = "You're fat."
+    | otherwise   = "You're a whale."
+
+-- You can do calcs in guards
+bmiTell' :: (RealFloat a) => a -> a-> String
+bmiTell' w h
+    | w / h ^ 2 <= 18.5 = "You're underweight."
+    | otherwise         = "blah blah..."
+
+-- Implement max functions
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b
+
+-- Implement ordering. Note that we can define a function as infix
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+-- Where clause, eliminate repetition:
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' w h
+    | bmi <= 18.5 = "You're skinny."
+    | bmi <= 25.0 = "You're normal..."
+    | otherwise   = "You're a whale."
+    where bmi = w / h ^ 2
+
+-- Where clause w/ multiple definitions
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' w h
+    | bmi <= skinny = "You're skinny."
+    | bmi <= normal = "You're normal..."
+    | otherwise   = "You're a whale."
+    where bmi = w / h ^ 2
+          skinny = 18.5
+          normal = 25.0
+
+{- You can do something like this also:
+...
+where bmi = weight / height ^ 2
+      (skinny, normal, fat) = (18.5, 25.0, 30.0)
+-}
+
+-- Another example of pattern matching in guards:
+
+initial :: String -> String -> String
+initials firstName lastName = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstName
+          (l:_) = lastName
+
+-- Stupid example, but it works.
+
+-- You can also define functions inside guards:
+calcBMIs :: (RealFloat a) -> [(a, a)] -> [a]
+calcBMIs xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+-- "bmi weight height" is an entirely new function
 
