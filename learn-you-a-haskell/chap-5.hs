3489e43d5576da435f2fedb0877ccac151182046
Mitch LeBlanc <supermitch@gmail.com>
2015-04-08 21:59:36 -0700

    Finished Chapter 5 on Recursion

--- learn-you-a-haskell/chap-5.hs
+++ learn-you-a-haskell/chap-5.hs
@@ -17,10 +17,50 @@ maximum'' (x:xs) = max x (maximum'' xs)
 
 -- implement replicate
 replicate' :: (Num i, Ord i) => i -> a -> [a]
-replacate' n x
-    | n <= 0    = 0
-    | othwerwise x:replicate' (n-1) x
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
 
 -- Note: 'Num' is not a subclass of 'Ord'... so we need to specify both
 -- when doing math AND comparison
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0    = []
+take' _ []      = []
+take' n (x:xs)  = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+-- Implement infinite recursive list:
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+-- Implement zip recursively.
+-- zip takes two lists, and generates tuples of elements
+-- until one of the lists is empty...
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+-- Elem returns True if an element is in a list
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = elem' a xs
+
+-- Quicksort in Haskell:
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallSorted = quicksort [a | a <- xs, a <= x]
+        bigSorted = quicksort [a | a <- xs, a > x]
+    in smallSorted ++ [x] ++ bigSorted
+
+-- Super chill.
 
