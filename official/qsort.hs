24c7223d51303ef3aee4c1faae3bd14132f7edd9
Mitch LeBlanc <mitch.leblanc@enernoc.com>
2015-04-10 10:39:14 -0700

    More Qsort examples

--- official/qsort.hs
+++ official/qsort.hs
@@ -1,7 +1,23 @@
 -- QSort implementation: file:///Library/Haskell/doc/ghc-doc/users_guide/ghci-debugger.html
 qsort [] = []
 qsort (a:as) = qsort left ++ [a] ++ qsort right
-    where (left,right) = (filter (<=a) as, filter (>a) as)
+    where (left, right) = (filter (<=a) as, filter (>a) as)
 
 main = print (qsort [8, 4, 0, 2, 1, 23, 11, 18])
+
+-- Alternative qsort:
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser = filter (< p) xs
+        greater = filter (>=p) xs
+
+-- List comprehension form
+
+qsort' :: Ord a => [a] -> [a]
+qsort' [] = []
+qsort' (p:xs) = qsort [x | x<-xs, x < p] ++ [p] ++ qsort [x | x<-xs, x >= p]
+
 
