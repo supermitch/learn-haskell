c8ee6dfed91562a427a0ac5d22addc2d502e5a77
Mitch LeBlanc <supermitch@gmail.com>
2015-10-29 19:23:43 -0700

    Finished bracket matching problem

--- sandbox/brackets.hs
+++ sandbox/brackets.hs
@@ -4,17 +4,19 @@ main = do
     putStrLn ("Your input was: " ++ input)
     putStrLn ("Brackets"  ++ (describeBool $ bracketMatch input) ++ " matched!")
 
-
 bracketMatch :: String -> Bool
-bracketMatch [] = error "Must provide a string input"
+bracketMatch [] = True
 bracketMatch [x] = False
 bracketMatch xs
-    | even $ length brackets = True
-    | odd $ length brackets = False
-    where brackets = filter (`elem` "()") xs
+    | -1 `elem` scan = False -- At any point a right bracket appears first
+    | last scan /= 0 = False -- Not all left brackets have been closed
+    | otherwise = True
+    where scan = countBrackets xs
 
+countBrackets :: String -> [Int]
+countBrackets xs = scanl (\acc x -> if x == '(' then acc + 1 else if x == ')' then acc - 1 else acc) 0 xs
 
 describeBool :: Bool -> String
 describeBool b
-    | False = " are not"
-    | True = " are"
+    | b == True = " are"
+    | otherwise = " are not"
