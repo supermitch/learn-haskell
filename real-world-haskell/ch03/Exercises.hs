3aa8c4a35cb25c440d013ef562dd6c6ace24c4a3
Mitch LeBlanc <supermitch@gmail.com>
2016-02-09 18:58:42 -0800

    Ch03 Ex. 6 & 7

--- real-world-haskell/ch03/Exercises.hs
+++ real-world-haskell/ch03/Exercises.hs
@@ -1,28 +1,29 @@
+import Data.List (sortBy)
+import Data.Function (on)
+
 -- file: ch03/Exercises.hs
 
 -- 1. Write a function to count number of elements in a list.
-
+-- 2. Add type signature
 counter :: [a] -> Int
 counter [] = 0
 counter (x:xs) = 1 + counter xs
 
--- 2. Add type signature
 
 -- 3. Write a function to compute mean of a list.
 --    (`fromIntegral` converts integer into floats)
-
 myMean :: (Fractional a) => [a] -> Maybe a
 myMean [] = Nothing
 myMean xs = Just (sum xs / fromIntegral (counter xs))
 
--- 4. Turn a list into a palindrome, e.g. [1..3] -> [1, 2, 3, 3, 2, 1]
 
+-- 4. Turn a list into a palindrome, e.g. [1..3] -> [1, 2, 3, 3, 2, 1]
 palindrome :: [a] -> [a]
 palindrome [] = []
 palindrome xs = xs ++ reverse xs
 
--- 5. Write a function to determine if a list is a palindrome.
 
+-- 5. Write a function to determine if a list is a palindrome.
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome [] = True
 isPalindrome [_] = True
@@ -35,3 +36,27 @@ isPalindrome' :: (Eq a) => [a] -> Bool
 isPalindrome' xs
     | xs == reverse xs = True
     | otherwise = False
+
+
+-- 6. Create a function that sorts lists of lists by length of inner list.
+--    Consider "sortBy" in Data.List
+lengthSort :: [[a]] -> [[a]]
+lengthSort xs = sortBy (compare `on` length) xs
+
+
+{- 7. Write a function intersperse' that operates like Python's join.
+      intersperse :: a -> [[a]] -> [a]
+      e.g.
+        ghci> intersperse ',' []
+        ""
+        ghci> intersperse ',' ["foo"]
+        "foo"
+        ghci> intersperse ',' ["foo","bar","baz","quux"]
+        "foo,bar,baz,quux"
+-}
+
+intersperse :: a -> [[a]] -> [a]
+intersperse _ []     = []
+intersperse y (x:[]) = x
+intersperse y (x:xs) = x ++ [y] ++ intersperse y xs
+
