d8cc83b5ce33a27e4f321d61f57a28d21cab6395
Mitch LeBlanc <supermitch@gmail.com>
2016-06-07 20:25:29 -0700

    Chapter 8 - Type Parameters

--- learn-you-a-haskell/8-chapter.hs
+++ learn-you-a-haskell/8-chapter.hs
@@ -120,4 +120,102 @@ Car {company = "Ford", model = "Mustang", year = 1967} -- displayed nicely
 
 -- no need to order fields in the Value Constructor call.
 
+-- 3. Type Parameters --
+------------------------
 
+-- A Value constructor takes some value parameters and then produces a new value.
+-- Similarly, a "Type Constructor" takes type parameters and produces a new type.
+
+data Maybe a = Nothing | Just a
+
+-- 'a' here is the "type parameter". 'Maybe' is a "type constructor".
+
+-- You can't have a value of type 'Maybe' because it's not a type; it's a type
+-- constructor.
+
+-- But if we pass Int to Just a, we get a Maybe Int.
+
+> Just "haha"
+Just "haha"
+> Just 64
+Just 64
+> :t Just "haha"
+Just "haha" :: Maybe [Char]
+> :t Just 84
+Just 84 :: (Num t) => Maybe t
+> :t Nothing
+Nothing :: Maybe a
+> Just 12 :: Maybe Double
+Just 10.0
+
+-- Note that Nothing is polymorphic! It can be Maybe Int if a function
+-- needed it to be, etc.
+
+-- An empty list [] is also polymorphic:
+
+>[1, 2, 3] ++ []
+[1, 2, 3]
+>["ha", "ha"] ++ []
+["ha", "ha"]
+
+-- If your type acts as a kind of "container" then it makes sense.
+-- You could define a Car this way but it makes no sense:
+
+data Car a b c = Car { company :: a
+                     , model :: b
+                     , year :: c
+                     } deriving (Show)
+
+-- Here's an example of parameterized type in Vector type.
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+-- and so on:
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+-- Note that there's no value in putting the Num constraint in the type declaration
+-- (though you can) because we have to repeat it in our functions anyway.
+
+-- Examples of usage:
+
+> Vector 3 5 8 `vplus` Vector 9 2 8
+Vector 12 7 16
+> Vector 4 9 5 `scalarMult` Vector 9.0 2.0 4.0
+74.0  -- Double!
+
+
+-- 4. Derived Instances (of a Type Class) --
+--------------------------------------------
+
+-- Haskell can automatically make our data type on of several typeclasses:
+-- Eq, Ord, Enum, Bounded, Show, and Read.
+
+-- You use the 'deriving' keyword when making our data type. For example:
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     } deriving (Eq)
+
+-- Now two people can be tested for equality.
+
+> let adRock = Person {firstName = "Adam", lastName = "Horovitz", age = 41}
+> let mca = Person {firstName = "Adam", lastName = "Yauch", age = 44}
+> mca == adRock
+False
+> mca == mca
+True
+
+-- Since person is now of Eq, we can use it for the arg to all functions that
+-- have a class constraint of Eq a in type signature.
+
+> let beastieBoys = [mca, adRock, mikeD]
+> mikeD `elem` beastieBoys
+True
+
+-- Show and read are for converting to and from Strings.
