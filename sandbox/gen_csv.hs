c2401b690a21ab0cc34893f18e2f8a41ba20ebba
Mitch LeBlanc <supermitch@gmail.com>
2017-11-09 21:11:18 -0800

    Nearly done gen_csv.hs

--- sandbox/gen_csv.hs
+++ sandbox/gen_csv.hs
@@ -1,15 +1,25 @@
 #!/usr/bin/env runhaskell
 import System.Environment
 import System.Random
-import System.Exit
+import qualified Data.UUID.V5 as U5
 
+
+stringify :: [(Int, Int)] -> [Char]
+stringify []     = ""
+stringify (x:xs) = (show $ fst x) ++ "," ++ (show $ snd x) ++ "\n" ++ (stringify xs)
 
 main = do
     putStrLn "Generating CSV file..."
     args <- getArgs
-    let file = head args
+    let fileName = head args
     let n = read $ last args :: Int
-    putStrLn $ "Writing <" ++ (head args) ++ "> lines to file <" ++ (last args) ++ ">"
+    putStrLn $ "Writing <" ++ (show n) ++ "> lines to file <" ++ (show fileName) ++ ">"
 
     g <- getStdGen  -- get a random number generator
-    print $ take n (randomRs (1::Int, 1000) g)
+    let rands = take n (randomRs (1::Int, 1000) g)
+
+    writeFile fileName "id,guid\n" -- write headers
+    appendFile fileName $ stringify $ zip [1..] rands
+
+    print $ U5.generateNamed
+
